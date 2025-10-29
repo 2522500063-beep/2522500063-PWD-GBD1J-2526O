bec9ca774cd90c49d50896fb6e3684018731b994
unknown <2522500063@mahasiswa.atmaluhur.ac.id>
2025-10-30 00:39:30 +0700

    membuat script.js

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,14 @@
+document.getElementById("menuToggle").addEventListener("click", function () {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("active");
+
+    if (nav.classList.contains("active")) {
+        this.textContent = "\u2716";
+    } else {
+        this.textContent = "\u2630";
+    }
+});
+
 document.querySelector("form").addEventListener("submit", function (e) {
     const nama = document.getElementById("txtNama").value.trim();
     const email = document.getElementById("txtEmail").value.trim();
